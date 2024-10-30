98ccd8307ea863a4f442fb335016f6d1a7ba892b
TheAutomator <32486959+fsoarezperu@users.noreply.github.com>
2024-10-30 10:51:22 -0500

    trial

--- app/api/[type]/[id]/route.ts
+++ app/api/[type]/[id]/route.ts
@@ -1,36 +1,29 @@
 import { NextResponse, NextRequest } from 'next/server';
 import prisma from '@/lib/prisma';
 
-export const config = {
-  runtime: 'edge', // si deseas que funcione en el runtime de Vercel Edge Functions
-};
-
 export async function DELETE(
   request: NextRequest,
   { params }: { params: { type: 'products' | 'customers' | 'orders'; id: string } }
 ) {
   try {
     const { type, id } = params;
+
     let result;
 
-    switch (type) {
-      case 'products':
-        result = await prisma.product.delete({
-          where: { id },
-        });
-        break;
-      case 'customers':
-        result = await prisma.customer.delete({
-          where: { id },
-        });
-        break;
-      case 'orders':
-        result = await prisma.order.delete({
-          where: { id },
-        });
-        break;
-      default:
-        return NextResponse.json({ error: 'Tipo inválido' }, { status: 400 });
+    if (type === 'products') {
+      result = await prisma.product.delete({
+        where: { id },
+      });
+    } else if (type === 'customers') {
+      result = await prisma.customer.delete({
+        where: { id },
+      });
+    } else if (type === 'orders') {
+      result = await prisma.order.delete({
+        where: { id },
+      });
+    } else {
+      return NextResponse.json({ error: 'Tipo inválido' }, { status: 400 });
     }
 
     return NextResponse.json(result);
